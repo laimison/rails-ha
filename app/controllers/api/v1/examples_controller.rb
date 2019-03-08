682ef43eaea9132018e2a62cfe7f9d56308b97bb
laimison <laimison1@gmail.com>
2019-03-08 00:29:37 +0000

    created "GET http://IP:PORT/v1/examples" to delete entry and "POST http://IP:PORT/v1/examples" to add entry - they are for read/write tests

--- app/controllers/api/v1/examples_controller.rb
+++ app/controllers/api/v1/examples_controller.rb
@@ -2,18 +2,17 @@ module Api::V1
   class ExamplesController < ApplicationController
     # GET http://IP:PORT/v1/examples
     def index
-      output = { example_get: params }
-      logger.info("I have received GET: #{output}")
-      render json: output
-      # render json: Example.all
+      # render json: params['test']
+      render json: Example.all
+
+      Example.last.delete if Example.any?
     end
 
     # POST http://IP:PORT/v1/examples
     def create
-      output = { example_get: params }
-      logger.info("I have received POST: #{output}")
+      object = Example.new(:string => 'test'); object.save
 
-      render json: output
+      render json: params
     end
   end
 end
