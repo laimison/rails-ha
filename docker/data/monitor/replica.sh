3e32a4df3abc01d16aec4758181cae2edc59f16c
laimison <laimison1@gmail.com>
2019-03-23 17:47:46 +0000

    at this point the health can be watched by mysql utilities, but needs access to manage failover

--- docker/data/monitor/replica.sh
+++ docker/data/monitor/replica.sh
@@ -4,10 +4,14 @@ case $* in
   status)
   mysqlrplshow --master=root:password@dolphin:33061 --discover-slaves-login=root:password --verbose
   ;;
-  install)
-  echo 2
+  health)
+  mysqlfailover --force --master=root:password@dolphin:33061 --discover-slaves-login=root:password --verbose health
+  ;;
+  failover)
+  mysqlfailover --force --master=root:password@dolphin:33061 --discover-slaves-login=root:password --verbose auto
+  echo "Now the tool is monitoring the health so master can go down..."
   ;;
   *)
-  echo 'Usage: install, status'
+  echo 'Usage: status, health, failover'
   ;;
 esac
