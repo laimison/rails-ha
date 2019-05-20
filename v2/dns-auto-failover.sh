d091abba346c1d6fc7cbbde73602239fcc6f9adb
laimison <laimison1@gmail.com>
2019-05-20 02:22:18 +0100

    added DNS auto failover

---
+++ v2/dns-auto-failover.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+while true
+do
+  # echo "iterating..."
+  > /tmp/mysql-ip
+
+  # Take only 2 instances
+  for instance in $(echo $MYSQL_INSTANCES | sed "s/,/ /g" | awk '{print $1" "$2}')
+  do
+    if my_host=`host $instance 2>/dev/null`
+    then
+      echo $my_host | awk '{print $NF}' >> /tmp/mysql-ip
+    else
+      echo $instance >> /tmp/mysql-ip
+    fi
+  done
+
+  for ip in `cat /tmp/mysql-ip`
+  do
+    if ! timeout 5 mysql -u app -p$MYSQL_PASSWORD -h $ip -s -N -e "SELECT @@global.read_only" >/dev/null 2>/dev/null
+    then
+      echo "Error: not able to do SELECT queries on $ip"
+    fi
+
+    if timeout 5 mysql -u app -p$MYSQL_PASSWORD -h $ip -s -N -e "SELECT @@global.read_only" | grep -q ^0$
+    then
+      master=$ip
+      replica=`cat /tmp/mysql-ip | grep -v ^$ip$ | head -n 1`
+      echo "mysql-master at $master and mysql-replica at $replica"
+
+      # Update entries if any changes in a cluster
+      if ! cat /etc/hosts | grep mysql-master | grep -q $master
+      then
+        echo "Detected changes in a cluster so replacing $master mysql-master"
+        cat /etc/hosts > /tmp/hosts && sed -i "/\smysql-master/d" /tmp/hosts && cat /tmp/hosts > /etc/hosts
+        echo "$master mysql-master" >> /etc/hosts
+      fi
+
+      if ! cat /etc/hosts | grep mysql-replica | grep -q $replica
+      then
+        echo "Detected changes in a cluster so replacing $replica mysql-replica"
+        cat /etc/hosts > /tmp/hosts && sed -i "/\smysql-replica/d" /tmp/hosts && cat /tmp/hosts > /etc/hosts
+        echo "$replica mysql-replica" >> /etc/hosts
+      fi
+
+      # Add entries if not already exists
+      if ! grep -q mysql-master /etc/hosts
+      then
+        echo "DNS records not found so adding $master mysql-master"
+        echo "$master mysql-master" >> /etc/hosts
+      fi
+
+      if ! grep -q mysql-replica /etc/hosts
+      then
+        echo "DNS records not found so adding $replica mysql-replica"
+        echo "$replica mysql-replica" >> /etc/hosts
+      fi
+
+      break # from for loop
+    fi
+  done
+
+  sleep 10
+done
--- v2/mysqlfailover_exec_after.sh
+++ v2/mysqlfailover_exec_after.sh
@@ -6,19 +6,16 @@ echo "`date` Running Commands After MySQL Failover"
 # curl -vsX GET "${CDN_API_ADDRESS}" -H "Content-Type: application/json" -H "X-Auth-Email: ${CDN_AUTH_EMAIL}" -H "X-Auth-Key: ${CDN_AUTH_KEY}" | jq .result > /tmp/CDN.json
 # logic to replace true to false in /tmp/CDN.json
 # curl -vsX PUT ${CDN_API_ADDRESS} -H "Content-Type: application/json" -H "X-Auth-Email: ${CDN_AUTH_EMAIL}" -H "X-Auth-Key: ${CDN_AUTH_KEY}" -d @/tmp/CDN.json
-
-sleep 10
+# sleep 10
 
 echo "Enable read-write on new master"
 docker exec -it mysql-2 bash -c 'mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SET GLOBAL read_only = OFF;"'
-
 sleep 10
 
-echo "Update mew master DNS record, remove slave"
-docker exec -it rails bash -c "cat /etc/hosts > /tmp/hosts && sed -i \"/\smysql-/d\" /tmp/hosts && cat /tmp/hosts > /etc/hosts"
-docker exec -it rails bash -c "host mysql-2 | awk '{print \$NF}' | while read -r line; do echo \"\$line mysql-master\" | tee -a /etc/hosts; done"
-
-sleep 10
+# echo "Update mew master DNS record, remove slave"
+# docker exec -it rails bash -c "cat /etc/hosts > /tmp/hosts && sed -i \"/\smysql-/d\" /tmp/hosts && cat /tmp/hosts > /etc/hosts"
+# docker exec -it rails bash -c "host mysql-2 | awk '{print \$NF}' | while read -r line; do echo \"\$line mysql-master\" | tee -a /etc/hosts; done"
+# sleep 10
 
 echo "When original master becomes available, make it read-only"
 while true
@@ -29,7 +26,7 @@ do
 
     echo "Make it as read-only instance"
     docker exec -it mysql-1 bash -c 'mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SET GLOBAL read_only = ON;"'
-    docker exec -it mysql-1 bash -c 'mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SELECT @@global.read_only"'
+    # docker exec -it mysql-1 bash -c 'mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SELECT @@global.read_only"'
 
     # echo "Here is the place for dump" | tee /dev/console | tee -a /tmp/mysqlfailover.log
 
